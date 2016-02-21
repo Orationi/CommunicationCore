b8a110cf091f553edc2895f36aa386691d951c36
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2016-02-21 10:02:01 +0300

    add description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommunicationCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core orationi system communication objects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Orationi")]
 [assembly: AssemblyProduct("Orationi")]
