45de4a9005233f806b2f096d70058e102008e67c
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2016-02-21 09:55:23 +0300

    Init nuget

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Orationi.CommunicationCore")]
+[assembly: AssemblyTitle("CommunicationCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Orationi.CommunicationCore")]
+[assembly: AssemblyCompany("Orationi")]
+[assembly: AssemblyProduct("Orationi")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Dmitry Pelevin")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
