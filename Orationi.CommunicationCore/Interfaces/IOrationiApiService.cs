68959b838d3604538677b11d1732ebc7edfba2b2
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2016-03-09 20:53:41 +0300

    #11 switch string to int

--- Orationi.CommunicationCore/Interfaces/IOrationiApiService.cs
+++ Orationi.CommunicationCore/Interfaces/IOrationiApiService.cs
@@ -5,37 +5,37 @@ using Orationi.CommunicationCore.Model;
 
 namespace Orationi.CommunicationCore.Interfaces
 {
-    [ServiceContract(Namespace = "https://orationi.org/master/Api/v1/", SessionMode = SessionMode.Required)]
-    public interface IOrationiApiService
-    {
-        [OperationContract]
-        string GetVersion();
+	[ServiceContract(Namespace = "https://orationi.org/master/Api/v1/", SessionMode = SessionMode.Required)]
+	public interface IOrationiApiService
+	{
+		[OperationContract]
+		string GetVersion();
 
-        [OperationContract]
-        OrationiSlaveItem[] GetSlavesList();
+		[OperationContract]
+		OrationiSlaveItem[] GetSlavesList();
 
-        [OperationContract]
-        ModuleItem[] GetModulesList();
+		[OperationContract]
+		ModuleItem[] GetModulesList();
 
-        [OperationContract]
-        ModuleVersionItem[] GetModuleVerionsList(int moduleId);
+		[OperationContract]
+		ModuleVersionItem[] GetModuleVerionsList(int moduleId);
 
-        [OperationContract]
-        ModuleItem RegisterModule(string moduleName);
+		[OperationContract]
+		ModuleItem RegisterModule(string moduleName);
 
-        [OperationContract]
-        AssignedModule[] UnregisterModule(int moduleId);
+		[OperationContract]
+		AssignedModule[] UnregisterModule(int moduleId);
 
-        [OperationContract]
-        AssignedModule AssignModule(Guid slaveId, int moduleId);
+		[OperationContract]
+		AssignedModule AssignModule(Guid slaveId, int moduleId);
 
-        [OperationContract]
-        void UnassignModule(Guid slaveId, int moduleId);
+		[OperationContract]
+		void UnassignModule(Guid slaveId, int moduleId);
 
-        [OperationContract]
-        void ExecutePowerShell(Guid slaveId, string script);
+		[OperationContract]
+		void ExecutePowerShell(Guid slaveId, string script);
 
-        [OperationContract]
-        void UploadModuleVersion(int moduleId, string major, string minor, string build, string revision, Stream packageStream);
-    }
+		[OperationContract]
+		void UploadModuleVersion(int moduleId, int major, int minor, int build, int revision, Stream packageStream);
+	}
 }
