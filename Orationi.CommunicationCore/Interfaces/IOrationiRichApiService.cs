475f580eba0b0b1528bdabec87f7560462fd686a
Dmitry Pelevin <dpelevin@navicons.ru>
2016-03-09 18:18:14 +0300

    #6 split api

---
+++ Orationi.CommunicationCore/Interfaces/IOrationiRichApiService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.ServiceModel;
+using Orationi.CommunicationCore.Model;
+
+namespace Orationi.CommunicationCore.Interfaces
+{
+	[ServiceContract(Namespace = "https://orationi.org/master/Api/v1/", SessionMode = SessionMode.Required)]
+	public interface IOrationiApiService
+	{
+		[OperationContract]
+		string GetVersion();
+
+		[OperationContract]
+		OrationiSlaveItem[] GetSlavesList();
+
+		[OperationContract]
+		ModuleItem[] GetModulesList();
+
+		[OperationContract]
+		ModuleVersionItem[] GetModuleVerionsList(string module);
+
+		[OperationContract]
+		ModuleItem RegisterModule(string moduleName);
+
+		[OperationContract]
+		AssignedModule[] UnregisterModule(string module);
+
+		[OperationContract]
+		AssignedModule AssignModule(string slave, string module);
+
+		[OperationContract]
+		void UnassignModule(string slave, string module);
+
+		[OperationContract]
+		void ExecutePowerShell(string slave, string script);
+
+		[OperationContract]
+		void UploadModuleVersion(string module, string major, string minor, string build, string revision, Stream packageStream);
+	}
+}
