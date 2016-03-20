fb08f9cfca3938be1b4636f9d1dc26661abc3fd1
Dmitry Pelevin <dpelevin@navicons.ru>
2016-03-20 22:17:50 +0300

    Add id of slave to slave configuration.

--- Orationi.CommunicationCore/Model/SlaveConfiguration.cs
+++ Orationi.CommunicationCore/Model/SlaveConfiguration.cs
@@ -1,17 +1,24 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace Orationi.CommunicationCore.Model
 {
-	/// <summary>
-	/// Provide information about slave configuration.
-	/// </summary>
-	[DataContract]
-	public class SlaveConfiguration
-	{
-		/// <summary>
-		/// Versions of slave-assigned modules.
-		/// </summary>
-		[DataMember]
-		public ModuleVersionItem[] Modules { get; set; }
-	}
+    /// <summary>
+    /// Provide information about slave configuration.
+    /// </summary>
+    [DataContract]
+    public class SlaveConfiguration
+    {
+        /// <summary>
+        /// Global slave Id.
+        /// </summary>
+        [DataMember]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Versions of slave-assigned modules.
+        /// </summary>
+        [DataMember]
+        public ModuleVersionItem[] Modules { get; set; }
+    }
 }
